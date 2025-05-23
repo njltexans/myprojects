93dd2cfa7463ebf23819943badbd7f7238efd76d
njltexans <njltexans@gmail.com>
2025-05-22 22:51:54 -0400

    Set up Flask app with SQLAlchemy and CORS

--- bestie.app/backend/app.py
+++ bestie.app/backend/app.py
@@ -2,3 +2,15 @@ from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
+import routes
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
